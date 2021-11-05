635d03d83e9da485ab70b592c26634326d5ed314
Mª Elena Téllez Santana <ElenaTellez@users.noreply.github.com>
2021-11-05 17:37:36 +0100

    Update javaBasico1.java

--- javaBasico1.java
+++ javaBasico1.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class JavaBasico1 {
+public class Main {
 
     public static void main(String[] args) {
         //Ejercicio Sesión 1 --> Para este primer ejercicio tendréis que realizar lo siguiente:
